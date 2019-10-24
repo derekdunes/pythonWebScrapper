dcdb95c22d4b9ab2fe8a4bf163f3fbd7fffa06c0
Mbah Derek <mbahderek@gmail.com>
2019-10-24 02:14:49 +0000

    update the script to have beautifulasoup

--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,18 @@
 from selenium import  webdriver
 from  BeautifulSoup import BeautifulSoup
-import pandas as pd
+#import pandas as pd
+
 
 driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
 
+driver.get("https://scholarship-positions.com/inti-postgraduate-merit-international-scholarship-in-malaysia/2019/10/18/")
+content = driver.page_source
+print(content)
 
 
+
+
+# soup = BeautifulSoup(content)
+# scholarTitle = soup.find('div', attrs={'class':'entry-title'})
+
+# print(scholarTitle)
