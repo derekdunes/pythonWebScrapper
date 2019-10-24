ef3270d1374297d2704499e2085a0bdea1b3cc4b
tobex001 <mbahtobex@gmail.com>
2019-10-24 07:46:40 +0100

    tested scraper

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,9 @@
 from selenium import  webdriver
-from  BeautifulSoup import BeautifulSoup
+#from  BeautifulSoup4 import BeautifulSoup
 #import pandas as pd
 
 
-driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
+driver = webdriver.Remote("http://localhost:4444/wd/hub", webdriver.DesiredCapabilities.HTMLUNIT.copy())
 
 driver.get("https://scholarship-positions.com/inti-postgraduate-merit-international-scholarship-in-malaysia/2019/10/18/")
 content = driver.page_source
