ac80f5397313a770257a3509e8c78e55103612cd
Mbah Derek <mbahderek@gmail.com>
2019-10-26 07:19:15 +0100

    removed phantomjs, added a webdriver chromeoption

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,13 @@
 from selenium import  webdriver
+from selenium.webdriver.chrome.options import Options
 from  bs4 import BeautifulSoup
 #import pandas as pd
 
-
-driver = webdriver.PhantomJS()
+chrome_options = Options()
+#chrome_options.add_argument("--disable-extensions")
+#chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(options=chrome_options)
 
 driver.get("https://scholarship-positions.com/inti-postgraduate-merit-international-scholarship-in-malaysia/2019/10/18/")
 content = driver.page_source
